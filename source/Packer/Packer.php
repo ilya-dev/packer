85ea75cd3b934cfc1aed0595d66cf20f39b5361c
Ilya S <public.ilya.s@gmail.com>
2014-06-19 22:43:53 +0700

    findAutoloader

--- source/Packer/Packer.php
+++ source/Packer/Packer.php
@@ -1,7 +1,17 @@
 <?php namespace Packer;
 
+use Symfony\Component\Console\Application;
+
 class Packer {
 
+    /**
+     * Run Packer.
+     *
+     * @return integer
+     */
+    public function run()
+    {
+        return (new Application())->run();
+    }
 
-
-} 
+}
