bde317d0d7fae62834b2068cd0fa35ac7093f016
Ilya S <public.ilya.s@gmail.com>
2014-06-19 22:51:33 +0700

    display name and version

--- source/Packer/Packer.php
+++ source/Packer/Packer.php
@@ -5,13 +5,20 @@ use Symfony\Component\Console\Application;
 class Packer {
 
     /**
+     * The version of Packer.
+     *
+     * @var string
+     */
+    const VERSION = 'early-development';
+
+    /**
      * Run Packer.
      *
      * @return integer
      */
     public function run()
     {
-        return (new Application())->run();
+        return (new Application('Packer', Packer::VERSION))->run();
     }
 
 }
