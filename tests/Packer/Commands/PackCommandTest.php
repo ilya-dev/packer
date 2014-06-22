331a42996df3f38e9d229ac608ff27d82ea23076
Ilya S <public.ilya.s@gmail.com>
2014-06-22 18:59:34 +0700

    messed up with the arguments

--- tests/Packer/Commands/PackCommandTest.php
+++ tests/Packer/Commands/PackCommandTest.php
@@ -7,7 +7,7 @@ class PackCommandTest extends \TestCase
     /** @test */ function it_receives_the_archive_name()
     {
         $tester = new CommandTester($command = new PackCommand);
-        $tester->execute([], ['destination' => 'packer']);
+        $tester->execute(['--destination' => 'packer']);
 
         $this->assertContains('The archive name you entered is packer.', $tester->getDisplay());
     }
