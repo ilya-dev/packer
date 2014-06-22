6aa4e2f98fa4c7eb1a1d5d26530f0b38f382690f
Ilya S <public.ilya.s@gmail.com>
2014-06-22 22:53:55 +0700

    write the test

--- tests/Packer/Commands/PackCommandTest.php
+++ tests/Packer/Commands/PackCommandTest.php
@@ -15,6 +15,9 @@ class PackCommandTest extends \TestCase
 
     /** @test */ function it_guesses_the_path_to_the_binary()
     {
+        $tester = new CommandTester(new PackCommand);
+        $tester->execute(['--destination' => 'packer.phar', '--source' => 'bin/packer']);
 
+        $this->assertContains('The binary file you specified is bin/packer.', $tester->getDisplay());
     }
 }
