5a057d13a09c6ac52622bddcb7536a96eabf39d2
Ilya S <public.ilya.s@gmail.com>
2014-06-22 23:43:51 +0700

    refactor

--- tests/Packer/Commands/PackCommandTest.php
+++ tests/Packer/Commands/PackCommandTest.php
@@ -4,20 +4,31 @@ use Symfony\Component\Console\Tester\CommandTester;
 
 class PackCommandTest extends \TestCase
 {
+    protected $tester;
+
+    protected $arguments = [
+        '--destination' => 'packer.phar',
+        '--source' => 'bin/packer'
+    ];
+
+    protected function setUp()
+    {
+        parent::setUp();
+
+        $this->tester = new CommandTester($this->subject);
+        $this->tester->execute($this->arguments);
+    }
+
     /** @test */ function it_receives_the_archive_name()
     {
-        $tester = new CommandTester(new PackCommand);
-        $tester->execute(['--destination' => 'packer.phar', '--source' => 'bin/packer']);
+        $output = $this->tester->getDisplay();
 
-        $this->assertContains('The archive name you entered is packer.', $tester->getDisplay());
-        $this->assertNotContains('.phar', $tester->getDisplay());
+        $this->assertContains('The archive name you entered is packer.', $output);
+        $this->assertNotContains('.phar', $output);
     }
 
     /** @test */ function it_guesses_the_path_to_the_binary()
     {
-        $tester = new CommandTester(new PackCommand);
-        $tester->execute(['--destination' => 'packer.phar', '--source' => 'bin/packer']);
-
-        $this->assertContains('The binary file you specified is bin/packer.', $tester->getDisplay());
+        $this->assertContains('The binary file you specified is bin/packer.', $this->tester->getDisplay());
     }
 }
