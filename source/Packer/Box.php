1b25b003a14f1ad1a6985c06a12079f6d6d4ced2
Ilya S <public.ilya.s@gmail.com>
2014-06-24 00:27:59 +0700

    represent configuration as Box object

---
+++ source/Packer/Box.php
@@ -0,0 +1,50 @@
+<?php namespace Packer;
+
+class Box implements \JsonSerializable {
+
+    protected $source, $destination, $files, $directories;
+
+    /**
+     * The constructor.
+     *
+     * @param string $source
+     * @param string $destination
+     * @param array $files
+     * @param array $directories
+     * @return Box
+     */
+    public function __construct($source, $destination, array $files, array $directories)
+    {
+        $this->source = $source;
+        $this->destination = $destination;
+        $this->files = $files;
+        $this->directories = $directories;
+    }
+
+    /**
+     * Represent the object as JSON.
+     *
+     * @return array
+     */
+    public function jsonSerialize()
+    {
+        $directories = [];
+
+        foreach ($this->directories as $directory)
+        {
+            $directories[] = [
+                'name' => '*.php',
+                'in' => $directory
+            ];
+        }
+
+        return [
+            'main' => $this->source,
+            'output' => $this->destination,
+            'stub' => true,
+            'files' => $this->files,
+            'finder' => $directories
+        ];
+    }
+
+}
